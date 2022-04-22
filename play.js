31bc8e29d9c9bdacd056224f7c51070579b224fa
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-22 23:02:29 +0000

    set up and created two functions to help send keyboard commands

--- play.js
+++ play.js
@@ -1,1 +1,24 @@
 const connect = require("./client")
+
+//stdin object returned will allow us to listen for keyboard input and react to it
+
+const setInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+  console.log("Game Closed");
+    process.exit();
+  }
+  
+
+};
+
+setInput();
+
