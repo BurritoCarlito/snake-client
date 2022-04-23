1f7b8cb04650e56f7e5176a676f810dc394522d8
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-23 00:38:49 +0000

    created constant.js for constant values such as IP address and PORT, also for mapping keys

---
+++ constant.js
@@ -0,0 +1,20 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const keyMap = {
+  MOVE_UP_KEY: '\w',
+  MOVE_DOWN_KEY: '\s',
+  MOVE_LEFT_KEY: '\a',
+  MOVE_RIGHT_KEY: '\d',
+  HAPPY_KEY: '\q',
+  HI_KEY: '\e',
+  BYE_KEY: '\h',
+  WOW_KEY: '\g',
+  BOO_KEY: '\c'
+};
+
+module.exports = {
+  IP,
+  PORT,
+  keyMap,
+};
