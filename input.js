30e3a12833953344d99f2a21a4682fcef0764fd4
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-23 00:01:53 +0000

    instead of console loggin to terminal the wasd keys when used will move the snake

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
-//stdin object returned will allow us to listen for keyboard input and react to it
+let connection; //stores the active TCP connection object.
 
-const setInput = function () {
+//stdin object returned will allow us to listen for keyboard input and react to it
+const setInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,13 +15,28 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
   console.log("Game Closed");
     process.exit();
-  };
+  }
+
+  if (key === '\w') {
+    connection.write("Move: up");
+  }
+
+  if (key === '\a') {
+    connection.write("Move: left");
+  }
+
+  if (key === '\s') {
+    connection.write("Move: down");
+  }
+
+  if (key === '\d') {
+    connection.write("Move: right");
+  }
   
 
 };
 
 
 module.exports = {
-  setInput,
-  handleUserInput,
+  setInput
 };
